fc13b49bb10f83b188d071ab3dfbbb2d0e84893c
pratikshakalbhor <kalbhorpratiksha333@gmail.com>
2026-03-08 14:41:53 +0530

    chore: Remove unused contract folders

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,6 @@ export const NETWORK_PASSPHRASE = Networks.TESTNET;
 export const RPC_URL = "https://soroban-testnet.stellar.org";
 export const HORIZON_URL = "https://horizon-testnet.stellar.org";
 
-export const CONTRACT_ID ="CDAV2WZS3H7TUM2QS7TQMQFI66EULYC7BD7NPPJYYNODCY4RLVHDT66D"; // <--- PASTE THE NEW ID HERE
+export const CONTRACT_ID ="CDLCKG5O54UL4C6KWCKFOEMAFXD6PXUEIMEEQCHMXBKLOFPIG5ALTE4O"; // <--- PASTE THE NEW ID HERE
 
 export const SOROBAN_SERVER = new rpc.Server(RPC_URL);
