4dc594d9b4ff6f7cf79018bf2a5ca3d5d683bc99
pratikshakalbhor <kalbhorpratiksha333@gmail.com>
2026-03-19 01:56:58 +0530

    fix: move Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqygelSUq8aaXw726qhsqHfgMYCmN-kT0",
-  authDomain: "freelancechain-5dfb1.firebaseapp.com",
-  projectId: "freelancechain-5dfb1",
-  storageBucket: "freelancechain-5dfb1.firebasestorage.app",
-  messagingSenderId: "125799745857",
-  appId: "1:125799745857:web:64cb0c7526d9896d02f9c0",
-  databaseURL: "https://freelancechain-5dfb1-default-rtdb.asia-southeast1.firebasedatabase.app"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
